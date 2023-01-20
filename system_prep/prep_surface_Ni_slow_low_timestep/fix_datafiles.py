302e0ae4f6f851910ceae98b7087cde2cc0f6b49
hmacdope <hugomacdermott@gmail.com>
2023-01-20 16:36:26 +1100

    clean up datafiles and get ready to make structures

---
+++ system_prep/prep_surface_Ni_slow_low_timestep/fix_datafiles.py
@@ -0,0 +1,17 @@
+import MDAnalysis as mda
+
+
+datafiles = ['quench' + str(i) + '.data' for i in range(1, 11)]
+
+for i, dfile in enumerate(datafiles):
+    print(dfile)
+    u = mda.Universe(dfile, atom_style='id type x y z')
+    ts = u.trajectory.ts
+    zslice = ts.positions[:, 2]
+    zmin = zslice.min()
+    zmax = zslice.max()
+    print(zmin, zmax)
+    ts.positions[:, 2] -= zmin
+    ts.dimensions[2] = zmax - zmin + 0.5
+    u.atoms.write(f'clean_{i+1}_{2*((i+1)*1000)}_O.data')
+    u.atoms.write(f'clean_{i+1}_{2*((i+1)*1000)}_O.pdb')

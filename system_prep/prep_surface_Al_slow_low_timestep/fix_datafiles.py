3636c9429038146718965cb6c77c39af1396d7dd
hmacdope <hugomacdermott@gmail.com>
2023-01-24 20:21:34 +1100

    add in production systems

--- system_prep/prep_surface_Al_slow_low_timestep/fix_datafiles.py
+++ system_prep/prep_surface_Al_slow_low_timestep/fix_datafiles.py
@@ -12,6 +12,6 @@ for i, dfile in enumerate(datafiles):
     zmax = zslice.max()
     print(zmin, zmax)
     ts.positions[:, 2] -= zmin
-    ts.dimensions[2] = zmax - zmin + 0.5
+    ts.dimensions[2] = zmax - zmin + 0.2
     u.atoms.write(f'clean_{i+1}_{2*((i+1)*1000)}_O.data')
     u.atoms.write(f'clean_{i+1}_{2*((i+1)*1000)}_O.pdb')

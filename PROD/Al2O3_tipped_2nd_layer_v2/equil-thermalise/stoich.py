444fc1e1c46be751c664eea81502ee8b5da55673
hmacdope <hugomacdermott@gmail.com>
2023-02-01 17:10:41 +1100

    investigate stoich and make Al2O3layer2

---
+++ PROD/Al2O3_tipped_2nd_layer_v2/equil-thermalise/stoich.py
@@ -0,0 +1,12 @@
+import ase
+
+from ase.io.lammpsdata import read_lammps_data
+
+a = read_lammps_data('Al2O3_tipped_2nd_layer.data', Z_of_type={1:13, 2:28, 3:8}, style='atomic', sort_by_id=True)
+cf = a.get_chemical_formula()
+# regex to find the number of each element
+import re
+# find the number of each element
+m = re.findall(r'([A-Z][a-z]*)(\d*)', cf)
+print(m)
+print("Ni/Al ratio = ", int(m[1][1])/int(m[0][1]))
